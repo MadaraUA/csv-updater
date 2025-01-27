ea541c5983a181be2bd9a12a13b15c7640ae2172
MadaraUA <87089194+MadaraUA@users.noreply.github.com>
2025-01-27 03:42:18 +0200

    Update update_csv.py

--- update_csv.py
+++ update_csv.py
@@ -24,15 +24,15 @@ def fetch_and_create_csv():
             [token_name, price_usd, price_change, volume_usd, reserve_usd]
         ]
 
-        # Открытие файла для записи
+        # Использование блока with
         with open("data.csv", "w", newline="", encoding="utf-8") as file:
             writer = csv.writer(file)  # Создание CSV writer
-            writer.writerows(csv_data)  # Запись данных в файл
+            writer.writerows(csv_data)  # Запись данных в CSV
 
         print("CSV файл обновлен.")
 
     except requests.RequestException as e:
         print(f"Ошибка при запросе данных: {e}")
 
-# Вызов функции для обновления CSV
+# Вызов функции
 fetch_and_create_csv()
